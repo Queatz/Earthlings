7b52ce4108c416cd02f53e457066330d93012c4a
JacobF <queatz@gmail.com>
2012-05-01 23:50:29 -0400

    edited in nano

--- site/static/script.js
+++ site/static/script.js
@@ -4,7 +4,7 @@ var manager;
 var server = '/a/' + 'xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
     return v.toString(16);
-});;
+});
 
 $(document).ready(function() {
 	var e;
